3e87e4b1e5bdf3e3b88a85c73b9f7e181bd87cb0
DoooongDong <rhkswls98@kakao.com>
2022-08-11 21:57:54 +0900

    8/11

---
+++ app/src/main/java/com/han/my_friend_kim_jung_san/ui/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.han.my_friend_kim_jung_san.ui.home
+
+import android.annotation.SuppressLint
+import android.app.usage.UsageEvents
+import android.content.Context
+import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.han.my_friend_kim_jung_san.databinding.FragmentHomeBinding
+import jp.co.recruit_mp.android.lightcalendarview.LightCalendarView
+import jp.co.recruit_mp.android.lightcalendarview.MonthView
+import jp.co.recruit_mp.android.lightcalendarview.WeekDay
+import jp.co.recruit_mp.android.lightcalendarview.accent.Accent
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+class HomeFragment : Fragment() {
+}
+
+
--- app/src/main/java/com/han/my_friend_kim_jung_san/ui/main/MainActivity.kt
+++ app/src/main/java/com/han/my_friend_kim_jung_san/ui/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.han.my_friend_kim_jung_san.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.han.my_friend_kim_jung_san.HomeFragment
+import com.han.my_friend_kim_jung_san.ui.home.HomeFragment
 import com.han.my_friend_kim_jung_san.R
 import com.han.my_friend_kim_jung_san.ui.account.AccountFragment
 import com.han.my_friend_kim_jung_san.databinding.ActivityMainBinding
