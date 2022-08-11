d09dffd1861dcc175447816ba928218636374fb7
dltnals1210 <dltnalstnals14@gmail.com>
2022-08-11 19:35:04 +0900

    모임방, 홈화면

--- app/src/main/java/com/han/my_friend_kim_jung_san/MainActivity.kt
+++ app/src/main/java/com/han/my_friend_kim_jung_san/MainActivity.kt
@@ -14,49 +14,56 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNav()
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment = CalendarFragment()
+        fragmentTransaction.add(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 
-    private fun initNav(){
-        binding.bottomNav.background = null
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayout, HomeFragment())
-            .commitAllowingStateLoss()
+        private fun initNav() {
+            binding.bottomNav.background = null
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, CalendarFragment())
+                .commitAllowingStateLoss()
 
-        binding.floatingButton.setOnClickListener {
-            Toast.makeText(applicationContext,"home", Toast.LENGTH_SHORT).show()
-        }
-
-        binding.bottomNav.setOnItemSelectedListener { item ->
-            when(item.itemId){
-                R.id.home -> {
-                    binding.floatingButton.setOnClickListener {
-                        Toast.makeText(applicationContext,"home", Toast.LENGTH_SHORT).show()
-                    }
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frameLayout, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.operations -> {
-                    binding.floatingButton.setOnClickListener {
-                        Toast.makeText(applicationContext,"oper", Toast.LENGTH_SHORT).show()
-                    }
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frameLayout, OperationsFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.account -> {
-                    binding.floatingButton.setOnClickListener {
-                        Toast.makeText(applicationContext,"account", Toast.LENGTH_SHORT).show()
-                    }
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frameLayout, AccountFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
+            binding.floatingButton.setOnClickListener {
+                Toast.makeText(applicationContext, "home", Toast.LENGTH_SHORT).show()
             }
-            false
+
+            binding.bottomNav.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> {
+                        binding.floatingButton.setOnClickListener {
+                            Toast.makeText(applicationContext, "home", Toast.LENGTH_SHORT).show()
+                        }
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, CalendarFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.operations -> {
+                        binding.floatingButton.setOnClickListener {
+                            Toast.makeText(applicationContext, "oper", Toast.LENGTH_SHORT).show()
+                        }
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, OperationsFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.account -> {
+                        binding.floatingButton.setOnClickListener {
+                            Toast.makeText(applicationContext, "account", Toast.LENGTH_SHORT).show()
+                        }
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, AccountFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                false
+            }
         }
     }
-}
