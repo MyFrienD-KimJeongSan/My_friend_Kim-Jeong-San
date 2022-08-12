cd6fb111cf17d9da8e260f63c9de71fc905d7cd4
DoooongDong <rhkswls98@kakao.com>
2022-08-12 20:22:50 +0900

    8/12

--- app/src/main/java/com/han/my_friend_kim_jung_san/ui/main/MainActivity.kt
+++ app/src/main/java/com/han/my_friend_kim_jung_san/ui/main/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.han.my_friend_kim_jung_san.ui.main
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.han.my_friend_kim_jung_san.ui.home.HomeFragment
 import com.han.my_friend_kim_jung_san.R
 import com.han.my_friend_kim_jung_san.ui.account.AccountFragment
 import com.han.my_friend_kim_jung_san.databinding.ActivityMainBinding
+import com.han.my_friend_kim_jung_san.ui.BaseActivity
+import com.han.my_friend_kim_jung_san.ui.account.SlideUpDialog
 import com.han.my_friend_kim_jung_san.ui.calculation.OperationsFragment
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun initAfterBinding() {
         initNav()
     }
-
     private fun initNav(){
         binding.bottomNav.background = null
         supportFragmentManager.beginTransaction()
@@ -50,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.account -> {
                     binding.floatingButton.setOnClickListener {
-                        Toast.makeText(applicationContext,"account", Toast.LENGTH_SHORT).show()
+                        onSlideUpDialog()
                     }
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.frameLayout, AccountFragment())
@@ -61,4 +63,14 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
+    @SuppressLint("InflateParams")
+    private fun onSlideUpDialog() {
+        var contentView: View = (getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.add_account_popup, null)
+        val slideupPopup = SlideUpDialog.Builder(this)
+            .setContentView(contentView)
+            .create()
+        slideupPopup.show()
+    }
+
+
 }
